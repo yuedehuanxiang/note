68258fc151295a7e3e37c901718f381b76f07338
chenshuai <384619043@qq.com>
2018-12-30 09:08:50 +0800

    swig和co模板渲染

--- nodejs/koa-demo/app-static.js
+++ nodejs/koa-demo/app-static.js
@@ -1,5 +1,6 @@
 const Koa = require("koa");
 const koaStaticCache = require("koa-static-cache");
+const Router = require("koa-router");
 const app = new Koa();
 
 app.use(
@@ -7,6 +8,47 @@ app.use(
     prefix: "/public" // 如果当前请求的url是以/public开始的，则作为静态资源请求
   })
 );
+
+const router = new Router();
+
+router.get("/", (ctx, next) => {
+  ctx.body = "hello";
+});
+
+// 动态路由参数
+router.get("/goods/:id", (ctx, next) => {
+  ctx.body = "商品名称：" + ctx.params.id;
+});
+
+// 重定向
+router.redirect("/admin", "/user", 301);
+
+// 子路由 写法1
+const userRouter = new Router();
+
+userRouter.get("/", (ctx, next) => {
+  ctx.body = "用户首页";
+});
+
+userRouter.get("/address", (ctx, next) => {
+  ctx.body = "用户收获地址";
+});
+
+router.use("/user", userRouter.routes());
+
+// 子路由 写法2
+const itemRouter = new Router({
+  prefix: "/item"
+});
+
+itemRouter.get("/add", (ctx, next) => {
+  ctx.body = "添加商品";
+});
+
+router.use(itemRouter.routes());
+
+// 把路由对象挂载到app对象中
+app.use(router.routes());
 
 app.use((ctx, next) => {});
 
